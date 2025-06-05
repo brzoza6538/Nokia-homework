f8804cc6b8dfe9ee37cfae93dff08de3508c829d
br2024 <m3.brzezinski@gmail.com>
2025-06-05 22:39:01 +0200

    adding FrameHandler to package

--- python_oop_and_testing/src/__init__.py
+++ python_oop_and_testing/src/__init__.py
@@ -2,3 +2,4 @@ from .GlobalVariables import *
 from .ContextMenu import ContextMenu
 from .DropdownElement import DropdownElement
 from .CustomKeywords import CustomKeywords
+from .FrameHandler import FrameHandler
